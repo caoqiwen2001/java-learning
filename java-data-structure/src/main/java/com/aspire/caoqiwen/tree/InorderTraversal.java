6398fc3b60b3b0fa2c14887a6ab5730fe23be318
luckyWen <727616488@qq.com>
2019-06-25 14:41:03 +0800

    InorderTraversal and PreorderTraversal java code

---
+++ java-data-structure/src/main/java/com/aspire/caoqiwen/tree/InorderTraversal.java
@@ -0,0 +1,68 @@
+package com.aspire.caoqiwen.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 给定一个二叉树，返回它的中序 遍历。
+ * 中序遍历是先遍历左子树，然后访问根节点，然后遍历右子树。
+ * 输入: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * 输出: [1,3,2]
+ */
+
+public class InorderTraversal {
+
+    public static class TreeNode {
+        int data;
+        InorderTraversal.TreeNode left;
+        InorderTraversal.TreeNode right;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public InorderTraversal.TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(InorderTraversal.TreeNode left) {
+            this.left = left;
+        }
+
+        public InorderTraversal.TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(InorderTraversal.TreeNode right) {
+            this.right = right;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return null;
+        }
+//        if ()
+
+
+        return list;
+    }
+}
---
+++ java-data-structure/src/main/java/com/aspire/caoqiwen/tree/PreorderTraversal.java
@@ -0,0 +1,76 @@
+package com.aspire.caoqiwen.tree;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个二叉树，返回它的 前序 遍历。
+ * 输入: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * 输出: [1,2,3]
+ */
+public class PreorderTraversal {
+
+    public static class TreeNode {
+        int data;
+        PreorderTraversal.TreeNode left;
+        PreorderTraversal.TreeNode right;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public PreorderTraversal.TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(PreorderTraversal.TreeNode left) {
+            this.left = left;
+        }
+
+        public PreorderTraversal.TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(PreorderTraversal.TreeNode right) {
+            this.right = right;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+//        preorderTraversal();
+    }
+
+    public static List<Integer> preorderTraversal(PreorderTraversal.TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        List<PreorderTraversal.TreeNode> nodelist = new LinkedList<>();
+        nodelist.add(root);
+        PreorderTraversal.TreeNode treeNode = null;
+        while (nodelist.size() > 0) {
+            treeNode = ((LinkedList<TreeNode>) nodelist).pop();
+            list.add(treeNode.data);
+            if (treeNode.right != null) {
+                ((LinkedList<TreeNode>) nodelist).push(treeNode.right);
+            }
+
+            if (treeNode.left != null) {
+                ((LinkedList<TreeNode>) nodelist).push(treeNode.left);
+            }
+        }
+        return list;
+    }
+
+}
