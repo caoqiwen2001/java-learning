68de5958cf65b9bab89165c278eab83f62b94945
luckyWen <727616488@qq.com>
2019-07-02 10:40:06 +0800

    PostorderTraversal add code

---
+++ java-data-structure/src/main/java/com/aspire/caoqiwen/tree/PostorderTraversal.java
@@ -0,0 +1,94 @@
+package com.aspire.caoqiwen.tree;
+
+import com.sun.org.apache.regexp.internal.REUtil;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PostorderTraversal {
+
+    public static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        List<TreeNode> nodeList = new LinkedList<>();
+        TreeNode cur = root;
+        TreeNode pre = null;
+        //左子树入栈。
+        while (cur != null || !nodeList.isEmpty()) {
+            while (cur != null) {
+                ((LinkedList<TreeNode>) nodeList).push(cur);
+                cur = cur.left;
+            }
+            cur = ((LinkedList<TreeNode>) nodeList).peek();
+            //防止右子树访问过了 又继续访问。
+            if (cur.right == null || cur.right == pre) {
+                ((LinkedList<TreeNode>) nodeList).pop();
+                list.add(cur.data);
+                pre = cur;
+                cur = null;
+            } else {
+                cur = cur.right;
+            }
+        }
+        return list;
+
+//        while (true) {
+//            while (root != null) {
+//                ((LinkedList<TreeNode>) nodeList).push(root);
+//                root = root.left;
+//            }
+//            if (nodeList.isEmpty()) {
+//                return list;
+//            }
+//            TreeNode node = ((LinkedList<TreeNode>) nodeList).peek();
+//            if (node.right != null) {
+//                root = node.right;
+//            } else {
+//                ((LinkedList<TreeNode>) nodeList).pop();
+//                list.add(node.data);
+//            }
+//            return list;
+//        }
+
+
+    }
+
+}
